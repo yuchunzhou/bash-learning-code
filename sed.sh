fb2ebfb30832a91cb180cd9410bc2323aa2b8ab5
Chunzhou Yu <enjoylife.czzz@gmail.com>
2025-08-24 11:43:04 +0800

    Update

--- sed.sh
+++ sed.sh
@@ -1,2 +1,9 @@
 # delete matched line
 sed -i '/sed/d' "$0"
+
+# replace matched string
+sed -i 's/sed/sedd/g' "$0"
+
+# find matched string with regular expression
+# hello111
+sed -i 's/hello[0-9]\+/hello world/g' "$0"
